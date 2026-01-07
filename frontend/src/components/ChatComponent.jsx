5906bb42880b4ec03b742f2a32601097a066122f
KimTaeyeon1111 <rlaxodus1001@naver.com>
2026-01-07 11:36:11 +0900

    1017_1136_kty

--- frontend/src/components/ChatComponent.jsx
+++ frontend/src/components/ChatComponent.jsx
@@ -3,7 +3,7 @@ import { useParams } from 'react-router-dom';
 import ReactMarkdown from 'react-markdown';
 import { TokenManager,protectedApi } from '../api/User_Api';
 
-import "../css/ChatComponent.css";
+import "./ChatComponent.css";
 
 const ChatComponent = () => {
     const { type } = useParams();
